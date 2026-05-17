66c29316c31518d803296f6c89a59bfb74f56601
Hellprint69 <helvinferyo@gmail.com>
2026-05-17 20:29:50 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Histats.com START (aync) - ID: 5019431 */}
+        {/* Histats.com  START  (aync) - ID: 5017612 */}
         <script 
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
               var _Hasync= _Hasync|| [];
-              _Hasync.push(['Histats.start', '1,5019431,4,0,0,0,00010000']);
+              _Hasync.push(['Histats.start', '1,5017612,4,0,0,0,00010000']);
               _Hasync.push(['Histats.fasi', '1']);
               _Hasync.push(['Histats.track_hits', '']);
               (function() {
@@ -29,7 +29,7 @@ export default function MyApp({ Component, pageProps }) {
       {/* Noscript Fallback */}
       <noscript>
         <a href="/" target="_blank">
-          <img src="//sstatic1.histats.com/0.gif?5019431&101" alt="histats" border="0" />
+          <img src="//sstatic1.histats.com/0.gif?5017612&101" alt="histats" border="0" />
         </a>
       </noscript>
     </>
